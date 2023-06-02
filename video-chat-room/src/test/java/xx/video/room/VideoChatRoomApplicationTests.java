5f8d44d4e13926f6d343cece1eac89bbabef2db2
wang.xianlin <14916253649wang>
2023-06-02 18:04:36 +0800

    update docs

---
+++ video-chat-room/src/test/java/xx/video/room/VideoChatRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package xx.video.room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoChatRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
