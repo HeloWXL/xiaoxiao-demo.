3bb894b7f9f1e4de0d1548dd820abf44f1f70a61
wang.xianlin <14916253649wang>
2023-06-05 10:46:01 +0800

    update docs

--- video-call/src/main/java/xx/call/controller/PageController.java
+++ video-call/src/main/java/xx/call/controller/PageController.java
@@ -7,9 +7,30 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class PageController {
 
-    @GetMapping("toPage")
-    public ModelAndView toPage() {
+    /**
+     * 跳转至一对一视频通话
+     * @return
+     */
+    @GetMapping("toCall")
+    public ModelAndView toCall() {
         return new ModelAndView("/call");
     }
 
+    /**
+     * 跳转至会议
+     * @return
+     */
+    @GetMapping("toMeeting")
+    public ModelAndView toMeeting() {
+        return new ModelAndView("/Meeting");
+    }
+
+    /**
+     * 跳转至直播
+     * @return
+     */
+    @GetMapping("toLive")
+    public ModelAndView toLive() {
+        return new ModelAndView("/Live");
+    }
 }
