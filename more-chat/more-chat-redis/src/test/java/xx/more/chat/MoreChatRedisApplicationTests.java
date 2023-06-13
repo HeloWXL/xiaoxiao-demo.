5ac39aaa8879dac2f90cfcbd6f6b8cb2d8985c40
wang.xianlin <756316064@qq.com>
2023-06-14 00:50:13 +0800

    update more chat

---
+++ more-chat/more-chat-redis/src/test/java/xx/more/chat/MoreChatRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package xx.more.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoreChatRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
